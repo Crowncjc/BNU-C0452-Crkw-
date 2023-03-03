1db8f367ae567e9291436303120e76f0535915b1
Crowncjc <114679613+Crowncjc@users.noreply.github.com>
2023-03-03 11:28:20 +0000

    Changes to dotnet and trial of terminal running.

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,5 +10,39 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        public static double FEET_IN_MILE = 5280; //Feet to Mile//
+
+        public static double METER_IN_MILE = 1609.344; //Meter to Mile//
+
+        public static double METER_IN_FEET = 3.28084; //Meter in Feet//
+
+        public double miles, meters, feet;
+
+        public void Run()
+        {
+            OutputHeading();
+            InputMiles();
+  
+        }
+        
+
+        private void OutputHeading()
+        {
+            Console.WriteLine();
+            Console.WriteLine(" =====================================================");
+            Console.WriteLine("    Distance Converter Application  by Crown Caesar   ");
+            Console.WriteLine(" =====================================================");
+            Console.WriteLine();
+        }
+
+        private void InputMiles()
+        {
+            Console.WriteLine("Please enter the number of Miles > ");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
+        }
+
+        
     }
 }
+
