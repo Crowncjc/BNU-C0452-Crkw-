a6f18ed6ff7ab49be23c9598184a4c89c2f5c105
Crowncjc <114679613+Crowncjc@users.noreply.github.com>
2023-03-10 22:10:16 +0000

    Added student grades properties and empty methods

--- ConsoleAppProject/App03/StudentGrades.cs
+++ ConsoleAppProject/App03/StudentGrades.cs
@@ -12,5 +12,62 @@ namespace ConsoleAppProject.App03
     public class StudentGrades
     {
 
+        public string [] Students { get; set; }
+
+        public int[] Marks { get; set; }
+
+        public int[] Gradeprofile { get; set; }
+
+        public double Mean { get; set; }
+
+        public int Minimum { get; set; }
+
+        public int Maximum { get; set; }
+
+    public StudentGrades()
+    {
+        Students = new string[]
+        {
+            "Emily","Ethan","Olivia","Liam","Sophia",
+            "Noah","Ava","William","Isabella","Benjamin"
+        };
+            Gradeprofile = new int[(int)Grades.A + 1];
+            Marks = new int[Students.Length];         
+    }
+        /// <summary>
+        /// 
+        /// 
+        /// </summary>
+        public void InputMarks()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// 
+        /// </summary>
+        public void OutputMarks()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// 
+        /// </summary>
+        public Grades ConvertToGrade(int mark)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// 
+        /// </summary>
+        public void CalculateStats()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
